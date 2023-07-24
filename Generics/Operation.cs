57ca6d05820eb21e86b09b19321f608942a15807
Narenthra Kishore RS <narenthrakishore.rs@tvsnext.com>
2023-07-24 16:21:57 +0530

    [Narenthra kishore] Add : largest of 3 Float Number.

--- Generics/Operation.cs
+++ Generics/Operation.cs
@@ -11,6 +11,15 @@
             if (c > a && c > b)
                 Console.WriteLine("c is largest");
         }
-        
+        public void FindMaxFloat(float a, float b, float c)
+        {
+            if (a > b && a > c)
+                Console.WriteLine("a is largest");
+            if (b > a && b > c)
+                Console.WriteLine("b is largest");
+            if (c > a && c > b)
+                Console.WriteLine("c is largest");
+        }
+
     }
 }
