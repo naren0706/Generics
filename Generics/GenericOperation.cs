1674144f902122183ac50ef680555a9d4319ae40
Narenthra Kishore RS <narenthrakishore.rs@tvsnext.com>
2023-07-24 17:34:29 +0530

    [Narenthra Kishore] Add : Generic comparison of maximum of 3 values

---
+++ Generics/GenericOperation.cs
@@ -0,0 +1,15 @@
+﻿namespace Generics
+{
+    internal class GenericOperation<T> where T : IComparable
+    {
+        public void FindMax(T a, T b, T c)
+        {
+            if (a.CompareTo(b) >= 0 && a.CompareTo(c) >= 0)
+                Console.WriteLine(a + " is Maximum Value");
+            if (b.CompareTo(a) >= 0 && b.CompareTo(c) >= 0)
+                Console.WriteLine(b + " is Maximum Value");
+            if (c.CompareTo(a) >= 0 && c.CompareTo(b) >= 0)
+                Console.WriteLine(c + " is Maximum Value");
+        }        
+    }
+}
--- Generics/Program.cs
+++ Generics/Program.cs
@@ -10,6 +10,10 @@ namespace Generics
             operation.FindMaxInteger(1, 2, 3);
             operation.FindMaxFloat(1f, 2f, 3f);
             operation.FindMaxString("hello","Hello","HELLO");
+
+            GenericOperation<int> operation2 = new GenericOperation<int>();
+
+            operation2.FindMax(1,2,3);
         }
     }
 }
